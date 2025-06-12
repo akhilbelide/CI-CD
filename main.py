87fe931db825c554a99dbe8fc2039c312e0a7ee2
Akhil <akhilbelide20@gmail.com>
2025-06-11 21:55:44 -0400

    Added a greet endpoint and changed the port

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI
 import uvicorn 
 
-app = FastAPI()
+app = FastAPI(
+    docs_url="/api-docs",
+    title = "My first Python App"
+    )
 
-@app.get("/")
+@app.get("/", tags = ["basic"])
 async def read_root():
     return {"message": "Hello, World!"}
 
+@app.get("/greet/{name}", tags = ["greet"])
+async def greet_user(name: str):
+    return {"message": f"Hello, {name}!"}
+
+
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8001)
